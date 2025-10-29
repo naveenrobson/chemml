bfed2cfb0e9296d82de923f92675207c3fcdaabe
naveenrobson <naveenrobson1999@gmail.com>
2025-10-28 18:53:24 -0700

    Update __init__.py

--- chemml/models/__init__.py
+++ chemml/models/__init__.py
@@ -9,8 +9,14 @@ from .mlp import MLP
 from .graphconvlayers import NeuralGraphHidden, NeuralGraphOutput
 # from .graphconvnetwork import build_graph_conv_model, build_graph_conv_net
 from .transfer import TransferLearning
+# from chemml.models.pytorchregressor import PyTorchRegressorWrapper
+from .pytorchregressor import PyTorchRegressorWrapper
+# from chemml.models.tensorflowregressor import TensorFlowRegressorWrapper
+from .tensorflowregressor import TensorFlowRegressorWrapper
+
 
 
 __all__ = [
-    'MLP', 'NeuralGraphHidden','NeuralGraphOutput','TransferLearning'
+    'MLP', 'NeuralGraphHidden','NeuralGraphOutput','TransferLearning','PyTorchRegressorWrapper', 'TensorFlowRegressorWrapper'
     ]
+
